51b0272d8a8339215e84002e0e20f5d25a0c2e55
koksingheww <koksing.hew@servicerocket.com>
2023-10-23 00:13:21 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+import Item from '@/components/Item'
+
+export default function Home() {
+  return (
+    <Item />
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Separator } from "@/components/ui/separator"
+
+export default function Header() {
+  return (
+    <>
+      <div className="flex justify-between items-center p-4 bg-white">
+          <h3 className="text-xl font-bold">Swapee</h3>
+          <NavigationMenu className="justify-end">
+          <NavigationMenuList className="flex space-x-4">
+              <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  All
+                  </NavigationMenuLink>
+              </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  New
+                  </NavigationMenuLink>
+              </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Trending
+                  </NavigationMenuLink>
+              </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Most Popular
+                  </NavigationMenuLink>
+              </Link>
+              </NavigationMenuItem>
+          </NavigationMenuList>
+          </NavigationMenu>
+      </div>
+      <Separator />
+    </>
+  )
+}
