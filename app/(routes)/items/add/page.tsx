5e932b6dddd9ee1d55c2f5b00b2a8eddede83089
koksing456 <koksing456@gmail.com>
2023-11-27 23:21:29 +0800

    check session for add item page

--- app/(routes)/items/add/page.tsx
+++ app/(routes)/items/add/page.tsx
@@ -1,5 +1,12 @@
 import { AddItemForm } from "@/app/components/item/add-item-form";
+import { readUserSession } from "@/app/lib/actions";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
+  const { data, error } = await readUserSession();
+
+  if (data.session === null) {
+    return redirect("/login");
+  }
   return <AddItemForm />;
 }
